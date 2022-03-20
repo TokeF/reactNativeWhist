59cc6ad7d2c0bfe7134a655456b0c359d1eb4a59
Toke <toke1993@hotmail.com>
2022-03-20 22:27:16 +0100

    added screen navigation

--- App.js
+++ App.js
@@ -1,25 +1,80 @@
 import React from 'react'
-import { StyleSheet, Button, Text, View, Alert, TouchableOpacity } from 'react-native';
+import { StyleSheet, SafeAreaView, TextInput, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <AppButton 
-        onPress={() => this.props.navigation.navigate('NewGameScreen')}
-        title="New Game"
-      />
-      <View style={{marginVertical: 10}} />
-      <AppButton
-        onPress={() => ""}
-        title="History"
-      />
-
-    </View>
-    
+    <NavigationContainer>
+     <Stack.Navigator   screenOptions={{headerShown: false}}>
+       <Stack.Screen name="Home" component={HomeScreen} />
+       <Stack.Screen name="Details" component={DetailsScreen} />
+     </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+        <AppButton 
+          onPress={() =>  navigation.navigate('Details')}
+          title="New Game"
+        />
+        <View style={{marginVertical: 10}} />
+        <AppButton
+          onPress={() => ""}
+          title="History"
+        />
+      </View>
+  )
+}
+
+const DetailsScreen = () => {
+	// this will be attached with each input onChangeText
+  const [textValue, setTextValue] = React.useState(''); 
+	// our number of inputs, we can add the length or decrease the length
+  const [numInputs, setNumInputs] = React.useState(1);
+  // all our input fields are tracked with this array
+  //const refInputs = React.useRef<[6]>([textValue]); 
+
+  const inputs = []
+  for (let i = 0; i < 5; i ++)
+{
+  inputs.push(
+    <View key={i} style={{flexDirection: 'row', alignItems: 'center'}}>
+      <Text>{i + 1}.</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={value => setInputValue(i, value)}
+        //value={refInputs.current[i]}
+        placeholder="placeholder"
+      />
+      {/* To remove the input */}
+      <AppButton onPress={() => removeInput(i)} style={{marginLeft: 5}}>
+        <AntDesign name="minuscircleo" size={20} color="red" />
+      </AppButton>
+    </View>
+  );
+}
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+			{inputs}
+			<AppButton onPress={addInput} style={styles.addButton}>
+				<Text style={{color: 'white', fontWeight: 'bold'}}>+ Add a new input</Text>
+			</AppButton>
+			<View style={{ marginTop: 25 }}>
+				<Text>You have answered:</Text>
+				{refInputs.current.map((value, i) => {
+					return <Text key={i} style={styles.answer}>{`${i+ 1} - ${value}`}</Text>
+				})} 
+			</View>
+		</ScrollView>
+  );
+};
 
 const AppButton = ({ onPress, title }) => (
   <TouchableOpacity onPress={onPress} style={styles.appButtonContainer} activeOpacity={0.8}>
@@ -54,5 +109,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     alignSelf: "center",
     textTransform: "uppercase"
-  }
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
